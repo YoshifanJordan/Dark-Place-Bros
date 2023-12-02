7a134748ea80f6a418b27969e0fbd079c46f0cc9
YoshifanJordan2468 <102327975+YoshifanJordan@users.noreply.github.com>
2023-12-02 11:30:34 -0500

    Added Rooms for the entrance and Part 1 of the City

---
+++ scripts/world/tilesets/GB_RetroCity.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="GB_RetroCity" tilewidth="40" tileheight="40" tilecount="130" columns="13">
+ <image source="../../../assets/sprites/tilesets/GB/GB_RetroCity.png" width="520" height="400"/>
+</tileset>
